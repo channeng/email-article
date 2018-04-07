8e5dedbc72070a39eb0960655176103bff7e00a7
Shannon <shannonchan@thecarousell.com>
2018-04-08 02:13:54 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ -e /home/ubuntu/email-article/database/app.db ] && [ -d /home/ubuntu/email-article/migrations/ ]; then
-    /home/ubuntu/.virtualenvs/env/bin/flask db upgrade
-elif [ -e /home/ubuntu/email-article/database/app.db ]; then
-    /home/ubuntu/.virtualenvs/env/bin/flask db migrate && \
-    /home/ubuntu/.virtualenvs/env/bin/flask db upgrade
-else
-    /home/ubuntu/.virtualenvs/env/bin/flask db init && \
-    /home/ubuntu/.virtualenvs/env/bin/flask db migrate && \
-    /home/ubuntu/.virtualenvs/env/bin/flask db upgrade
+if [ -d /home/ubuntu/email-article/migrations/ ]; then
+	rm -rf /home/ubuntu/email-article/migrations
 fi
+
+if [ -e /home/ubuntu/email-article/database/app.db ]; then
+	sqlite3 mydatabase.db < /home/ubuntu/email-article/delete_alembic_version.sqlite
+fi
+
+/home/ubuntu/.virtualenvs/env/bin/flask db init && \
+/home/ubuntu/.virtualenvs/env/bin/flask db migrate && \
+/home/ubuntu/.virtualenvs/env/bin/flask db upgrade
 
 /home/ubuntu/.virtualenvs/env/bin/gunicorn --bind 0.0.0.0:5000 --worker-class eventlet --workers 1 main:app
