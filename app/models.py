e5c6f4a230aae2d324bdfe5a8ebc0dcbda11cf44
Shannon <shannonchan@thecarousell.com>
2019-06-20 02:14:23 +0800

    Undo change for username as index field

--- app/models.py
+++ app/models.py
@@ -28,7 +28,7 @@ class Role(db.Model, RoleMixin):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(255), index=True, unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean(), default=True)
