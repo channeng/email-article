e713d5f6161ab8cd8140132b05dbf7d71d03dc99
Shannon <shannonchan@thecarousell.com>
2019-06-25 01:03:27 +0800

    Update gmail API token

--- connect/google_api.py
+++ connect/google_api.py
@@ -1,33 +1,32 @@
 from __future__ import print_function
-import httplib2
+
 import os
+import pickle
 
 from apiclient import discovery
-from oauth2client import client, tools
-from oauth2client.file import Storage
+from oauth2client import tools
 
 from config import Config
-
-_CREDENTIAL_DIR = Config.CREDENTIAL_DIR
-_CLIENT_SECRET_FILE = Config.CLIENT_SECRET_FILE
-
 try:
     import argparse
     flags = argparse.ArgumentParser(
-        parents=[tools.argparser]).parse_known_args()[0]
+        parents=[tools.argparser]).parse_known_args()
 except ImportError:
     flags = None
+
+
+CREDENTIAL_DIR = Config.CREDENTIAL_DIR
+CREDENTIALS_FILE = Config.CREDENTIALS_FILE
+
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
 SCOPES = [
-    'https://www.googleapis.com/auth/gmail.compose',
-    'https://www.googleapis.com/auth/calendar.readonly'
+    'https://mail.google.com/',
 ]
-APPLICATION_NAME = Config.APPLICATION_NAME
 
 
-def get_credentials():
+def get_credentials(name):
     """Gets valid user credentials from storage.
 
     If nothing has been stored, or if the stored credentials are invalid,
@@ -36,39 +35,27 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    if not os.path.exists(_CREDENTIAL_DIR):
-        os.makedirs(_CREDENTIAL_DIR)
+    if not os.path.exists(CREDENTIAL_DIR):
+        os.makedirs(CREDENTIAL_DIR)
     credential_path = os.path.join(
-        _CREDENTIAL_DIR, APPLICATION_NAME.lower().replace(" ", "_") + '.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(
-            os.path.join(_CREDENTIAL_DIR, _CLIENT_SECRET_FILE), SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
+        CREDENTIAL_DIR, CREDENTIALS_FILE[name])
+    with open(credential_path, 'rb') as token:
+        credentials = pickle.load(token)
 
     return credentials
 
 
-def _build_service(api, version):
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
+def _build_service(api, version, name="pocket_sg_hello"):
+    credentials = get_credentials(name)
     return discovery.build(
-        api, version,
-        http=http,
+        api, version, credentials=credentials,
         # cache_discovery=False to silence cache error
         # https://github.com/google/google-api-python-client/issues/299
         cache_discovery=False)
 
 
-def get_gmail_service():
-    service = _build_service('gmail', 'v1')
+def get_gmail_service(name="pocket_sg_hello"):
+    service = _build_service('gmail', 'v1', name=name)
     return service
 
 
